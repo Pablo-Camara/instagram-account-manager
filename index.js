c0321d19b393ff9e7edc18fa4ed61358609fdcf1
Pablo Camara <pablo.camara.pt@gmail.com>
2020-05-23 10:41:01 +0100

    progress

--- index.js
+++ index.js
@@ -7,20 +7,19 @@ window.igMasterTool = {
 	getQueryUrl: function(queryHash,variables){
 		return 'https://www.instagram.com/graphql/query/?query_hash=' + queryHash + '&variables=' + variables;
 	},
+	getUnfollowUrl: function(accountId){
+		return 'https://www.instagram.com/web/friendships/' + accountId + '/unfollow/';
+	},
 	initialize: function(){
-		this.addAccount('31680581864','camara.pt');
-		this.addAccount('35537458504','edge.of.the.raw.moment');
-		
+		this.addAccount('camara.pt','31680581864');
+	/* 	this.addAccount('edge.of.the.raw.moment','35537458504'); */
 	
 		this.getDataForAllAccounts();
 	},
-	addAccount: function(accountId,username = null){
-		if(!accountId){
-			alert('Account ID required!!');
-			return;
-		}
+	addAccount: function(username,accountId){
 
-		this.accountsData[accountId] = {
+		this.accountsData[username] = {
+			id: accountId,
 			username: username,
 			followers: {
 				list: [],
@@ -36,19 +35,20 @@ window.igMasterTool = {
 					endCursor: null
 				}
 			},
-			accountsNotFollowingBack: []
+			accountsNotFollowingBack: [],
+			unfollowAttempts: []
 		};
 	},
 	getDataForAllAccounts: function(){
-		for (var accountId in this.accountsData) {
-		    if (Object.prototype.hasOwnProperty.call(this.accountsData, accountId)) {
-		        this.getFollowers(accountId);
-		        this.getFollowing(accountId);
+		for (var username in this.accountsData) {
+		    if (Object.prototype.hasOwnProperty.call(this.accountsData, username)) {
+		        this.getFollowers(username);
+		        this.getFollowing(username);
 		    }
 		}
 	},
-	getFollowers: function(accountId,after){
-		const variables = '{"id":"'+accountId+'", "first": 50'+ (typeof after === "string" ? ', "after": "'+after+'"' : '') +'}';
+	getFollowers: function(username,after){
+		const variables = '{"id":"'+this.accountsData[username].id+'", "first": 50'+ (typeof after === "string" ? ', "after": "'+after+'"' : '') +'}';
 		const url = this.getQueryUrl(this.queryHashes.getFollowers,variables);
 		fetch(url).then(function(data){
 		    const reader = data.body.getReader();
@@ -58,61 +58,61 @@ window.igMasterTool = {
 						const jsonResult = JSON.parse(result);
 						const followers = jsonResult.data.user.edge_followed_by;
 
-						if( !(typeof window.igMasterTool.accountsData[accountId] === 'object') ){
-							window.igMasterTool.addAccount(accountId);
+						if( !(typeof window.igMasterTool.accountsData[username] === 'object') ){
+							window.igMasterTool.addAccount(username);
 						}
 				
-						window.igMasterTool.accountsData[accountId].followers.list.push.apply(
-							window.igMasterTool.accountsData[accountId].followers.list,
+						window.igMasterTool.accountsData[username].followers.list.push.apply(
+							window.igMasterTool.accountsData[username].followers.list,
 							Array.from(followers.edges)
 						);
 
 						// TODO: Better way to show feedback to the user using the script:
-						console.log('Total followers for account '+ window.igMasterTool.accountsData[accountId].username +' collected so far: ' + window.igMasterTool.accountsData[accountId].followers.list.length);
+						console.log('Total followers for account '+ username +' collected so far: ' + window.igMasterTool.accountsData[username].followers.list.length);
 						
 
 						if(followers.page_info.end_cursor){
-							window.igMasterTool.accountsData[accountId].followers.fetchInfo.endCursor = followers.page_info.end_cursor;
+							window.igMasterTool.accountsData[username].followers.fetchInfo.endCursor = followers.page_info.end_cursor;
 						}
 						
 						if(followers.page_info.has_next_page){
-							window.igMasterTool.accountsData[accountId].followers.fetchInfo.hasNextPage = true;
+							window.igMasterTool.accountsData[username].followers.fetchInfo.hasNextPage = true;
 
 							// TODO: Better way to show feedback to the user using the script:
-							console.log('Fetching next followers page for account '+ window.igMasterTool.accountsData[accountId].username +'..')
+							console.log('Fetching next followers page for account '+ username +'..')
 
 							setTimeout(function(){
 								window.igMasterTool.getFollowers(
-									accountId,
-									window.igMasterTool.accountsData[accountId].followers.fetchInfo.endCursor
+									username,
+									window.igMasterTool.accountsData[username].followers.fetchInfo.endCursor
 								);
 							}, 2000);
 						} else {
 							// if hasNextPage === true then still fetching
-							window.igMasterTool.accountsData[accountId].followers.fetchInfo.hasNextPage = null;
+							window.igMasterTool.accountsData[username].followers.fetchInfo.hasNextPage = null;
 
 							// Must reset endCursor so we are able to re-fetch followers from scratch:
-							window.igMasterTool.accountsData[accountId].followers.fetchInfo.endCursor = null;
+							window.igMasterTool.accountsData[username].followers.fetchInfo.endCursor = null;
 
 							// TODO: Better way to show feedback to the user using the script:
-							console.log('Done fetching followers for account '+ window.igMasterTool.accountsData[accountId].username +'!!');
+							console.log('Done fetching followers for account '+ username +'!!');
 						}
 					} catch(err){
 						setTimeout(function(){
 								window.igMasterTool.getFollowers(
-									accountId,
-									window.igMasterTool.accountsData[accountId].followers.fetchInfo.endCursor
+									username,
+									window.igMasterTool.accountsData[username].followers.fetchInfo.endCursor
 								);
 						}, 1000);
 
 						// TODO: Better way to show feedback to the user using the script:
-						console.log('Failed attempt..No worries..This is normal..Retrying..( Account '+ window.igMasterTool.accountsData[accountId].username +' )');
+						console.log('Failed attempt..No worries..This is normal..Retrying..( Account '+ username +' )');
 					}
 			});
 		});
 	},
-	getFollowing: function(accountId,after){
-		const variables = '{"id":"'+accountId+'", "first": 50'+ (typeof after === "string" ? ', "after": "'+after+'"' : '') +'}';
+	getFollowing: function(username,after){
+		const variables = '{"id":"'+this.accountsData[username].id+'", "first": 50'+ (typeof after === "string" ? ', "after": "'+after+'"' : '') +'}';
 		const url = this.getQueryUrl(this.queryHashes.getFollowing,variables);
 		fetch(url).then(function(data){
 		    const reader = data.body.getReader();
@@ -122,75 +122,111 @@ window.igMasterTool = {
 						const jsonResult = JSON.parse(result);
 						const following = jsonResult.data.user.edge_follow;
 
-						if( !(typeof window.igMasterTool.accountsData[accountId] === 'object') ){
-							window.igMasterTool.addAccount(accountId);
+						if( !(typeof window.igMasterTool.accountsData[username] === 'object') ){
+							window.igMasterTool.addAccount(username);
 						}
 				
-						window.igMasterTool.accountsData[accountId].following.list.push.apply(
-							window.igMasterTool.accountsData[accountId].following.list,
+						window.igMasterTool.accountsData[username].following.list.push.apply(
+							window.igMasterTool.accountsData[username].following.list,
 							Array.from(following.edges)
 						);
 
 						// TODO: Better way to show feedback to the user using the script:
-						console.log('Total accounts that '+ window.igMasterTool.accountsData[accountId].username +' follows collected so far: ' + window.igMasterTool.accountsData[accountId].following.list.length);
+						console.log('Total accounts that '+ username +' follows collected so far: ' + window.igMasterTool.accountsData[username].following.list.length);
 						
 
 						if(following.page_info.end_cursor){
-							window.igMasterTool.accountsData[accountId].following.fetchInfo.endCursor = following.page_info.end_cursor;
+							window.igMasterTool.accountsData[username].following.fetchInfo.endCursor = following.page_info.end_cursor;
 						}
 						
 						if(following.page_info.has_next_page){
-							window.igMasterTool.accountsData[accountId].following.fetchInfo.hasNextPage = true;
+							window.igMasterTool.accountsData[username].following.fetchInfo.hasNextPage = true;
 
 							// TODO: Better way to show feedback to the user using the script:
-							console.log('Fetching next page for the accounts the user ' + window.igMasterTool.accountsData[accountId].username + ' is following..')
+							console.log('Fetching next page for the accounts the user ' + username + ' is following..')
 
 							setTimeout(function(){
 								window.igMasterTool.getFollowing(
-									accountId,
-									window.igMasterTool.accountsData[accountId].following.fetchInfo.endCursor
+									username,
+									window.igMasterTool.accountsData[username].following.fetchInfo.endCursor
 								);
 							}, 2000);
 						} else {
 							// if hasNextPage === true then still fetching
-							window.igMasterTool.accountsData[accountId].following.fetchInfo.hasNextPage = null;
+							window.igMasterTool.accountsData[username].following.fetchInfo.hasNextPage = null;
 
 							// Must reset endCursor so we are able to re-fetch following accs from scratch:
-							window.igMasterTool.accountsData[accountId].following.fetchInfo.endCursor = null;
+							window.igMasterTool.accountsData[username].following.fetchInfo.endCursor = null;
 
 							// TODO: Better way to show feedback to the user using the script:
-							console.log('Done fetching accounts that the user '+ window.igMasterTool.accountsData[accountId].username +' follows!!');
+							console.log('Done fetching accounts that the user '+ username +' follows!!');
 						}
 					} catch(err){
 						setTimeout(function(){
 								window.igMasterTool.getFollowing(
-									accountId,
-									window.igMasterTool.accountsData[accountId].following.fetchInfo.endCursor
+									username,
+									window.igMasterTool.accountsData[username].following.fetchInfo.endCursor
 								);
 						}, 1000);
 
 						// TODO: Better way to show feedback to the user using the script:
-						console.log('Failed attempt..No worries..This is normal..Retrying.. ( Account '+ accountId +' )');
+						console.log('Failed attempt..No worries..This is normal..Retrying.. ( Account '+ username +' )');
 					}
 			});
 		});
 	},
-	getUsersNotFollowingBack: function(accountId){
+	getUsersNotFollowingBack: function(username,attemptToUnfollowUnfollowers){
 		var accountsNotFollowingBack = [];
-		for(var x = 0; x < this.accountsData[accountId].following.list.length; x++){
-			var userBeingFollowed = this.accountsData[accountId].following.list[x].node;
+		for(var x = 0; x < this.accountsData[username].following.list.length; x++){
+			var userBeingFollowed = this.accountsData[username].following.list[x].node;
 			var followsBack = false;
-			for(var y = 0; y < this.accountsData[accountId].followers.list.length; y++){
-				var userFollowingBack = this.accountsData[accountId].followers.list[y].node;
+			// For each user that I follow, I will check if he also is in my followers list:
+			for(var y = 0; y < this.accountsData[username].followers.list.length; y++){
+				var userFollowingBack = this.accountsData[username].followers.list[y].node;
 				if(userBeingFollowed.username === userFollowingBack.username){
 					followsBack = true;
+					break;
 				}
 			}
 
 			if(false === followsBack){
 				accountsNotFollowingBack.push(userBeingFollowed);
+				continue;
 			}
 		}
-		this.accountsData[accountId].accountsNotFollowingBack = accountsNotFollowingBack;
+		this.accountsData[username].accountsNotFollowingBack = accountsNotFollowingBack;
+
+		
+		if(attemptToUnfollowUnfollowers){
+			for(var z = 0; z < accountsNotFollowingBack.length; z++){
+				//TODO: Better way to show the feedback to the user using my script 
+				const secondsBetweenUnfollows = 30000;
+				console.log('Will attempt to unfollow: ' + accountsNotFollowingBack[z].username + ' in ' + (((z+1) * secondsBetweenUnfollows)/1000)) + ' second(s)..';
+				setTimeout(window.igMasterTool.unfollowAccount,z * secondsBetweenUnfollows,accountsNotFollowingBack[z],username); 
+			}
+		}
+	},
+	unfollowAccount(accountToUnfollow,usernameFromUnfollower){
+		const url = window.igMasterTool.getUnfollowUrl(accountToUnfollow.id);
+		fetch(url,{
+			"headers": {
+				"X-CSRFToken": "x5nXAfhzVsjAoNX0b5jSysr3JYjgotud"
+			},
+			"method": "POST"
+		}).then(function(response){
+			return response.json();
+		}).then(function(data){
+			window.igMasterTool.accountsData[usernameFromUnfollower].unfollowAttempts.push({
+				account: accountToUnfollow,
+				result: data
+			});
+			// TODO: Improve feed back: create gui through javascript
+			if(data.status == 'ok'){
+				console.log('Unfollowed < ' + accountToUnfollow.username + ' > successfully.');
+				return;
+			} 
+
+			console.log('Failed to unfollow < ' + accountToUnfollow.username + ' >.');
+		});
 	}
 };
